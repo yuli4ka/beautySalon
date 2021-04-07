47902a644078ddaceef1c9469a7a406528366587
Yuliia Zhytariuk <yuli4kazhitaryuk@gmail.com>
2021-04-07 13:17:02 +0300

    Initial commit

---
+++ src/test/java/io/mathlina/beautysalon/BeautySalonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mathlina.beautysalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
