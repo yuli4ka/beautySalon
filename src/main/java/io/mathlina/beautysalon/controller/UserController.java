4862d598ceb801ca936b3ea3ffe35b62fe63fe38
Yuliia Zhytariuk <yuli4kazhitaryuk@gmail.com>
2021-04-12 22:09:47 +0300

    Add message on empty old password during password update

--- src/main/java/io/mathlina/beautysalon/controller/UserController.java
+++ src/main/java/io/mathlina/beautysalon/controller/UserController.java
@@ -1,7 +1,6 @@
 package io.mathlina.beautysalon.controller;
 
 import io.mathlina.beautysalon.domain.User;
-import io.mathlina.beautysalon.dto.MasterDto;
 import io.mathlina.beautysalon.dto.UserProfileDto;
 import io.mathlina.beautysalon.exception.CannotSaveUserToDatabase;
 import io.mathlina.beautysalon.exception.EmailIsAlreadyTaken;
@@ -10,13 +9,9 @@ import io.mathlina.beautysalon.exception.WrongPassword;
 import io.mathlina.beautysalon.service.UserService;
 import io.mathlina.beautysalon.validation.PasswordEqualityValidatorProfile;
 import javax.validation.Valid;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -46,6 +41,7 @@ public class UserController {
 
   //TODO: add message for code activation, user not activated
   //TODO: add 'send activation code again' button
+  //TODO: add message for successful changes
   @PostMapping("/profile")
   public String updateProfile(@RequestParam("oldPassword") String oldPassword,
       @ModelAttribute("userProfileDto")
@@ -54,6 +50,10 @@ public class UserController {
 
     PasswordEqualityValidatorProfile validator = new PasswordEqualityValidatorProfile();
     validator.validate(userProfileDto, bindingResult);
+
+    if (oldPassword.isEmpty() && !userProfileDto.getPassword().isEmpty()) {
+      bindingResult.rejectValue("password", "fill.the.field.if.password.change");
+    }
 
     if (!bindingResult.hasErrors()) {
       try {
